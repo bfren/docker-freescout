ccb699f81a1718c81835b9b8322f0a52cf67e0d6
bfren <ben@bcgdesign.com>
2022-03-20 12:40:01 +0000

    v3.3.0 (#63)
    Major updates
    * Adding PHP 7.4 and PHP 8.0 specific builds

---
+++ generate-dockerfiles.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -euo pipefail
+
+docker pull bfren/alpine
+
+BASE_REVISION="4.2.3"
+echo "Base: ${BASE_REVISION}"
+
+PHP_VERSIONS="7.4 8.0"
+for V in ${PHP_VERSIONS} ; do
+
+    echo "WordPress for PHP ${V}"
+
+    DOCKERFILE=$(docker run \
+        -v ${PWD}:/ws \
+        -e BF_DEBUG=0 \
+        bfren/alpine esh \
+        "/ws/Dockerfile.esh" \
+        BASE_REVISION=${BASE_REVISION} \
+        PHP_MINOR=${V}
+    )
+
+    echo "${DOCKERFILE}" > ./php${V}/Dockerfile
+
+done
+
+docker system prune -f
+echo "Done."
